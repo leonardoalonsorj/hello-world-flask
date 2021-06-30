9db5d5e6d20c5ef92284b1ac22cc0ec1be684f25
Leonardo Alonso <leonardoalonso.rj@gmail.com>
2021-06-30 16:35:00 -0300

    Sample Flask API v2

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_openshift ():
-return 'Python + Flask - Test application to run on openshift'
+    return 'Python + Flask - Test application to run on openshift'
 
 app.run(host='0.0.0.0', port =8080)
